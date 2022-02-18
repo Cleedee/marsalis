53322c260dba3d5d9336996e3524457b360ebe98
Claudio Torcato <claudiotorcato@gmail.com>
2022-02-18 00:10:27 -0300

    modelo Usuario e Produto

--- marsalis/servico/admin.py
+++ marsalis/servico/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Usuario, Produto
+
+admin.site.register(Usuario)
+admin.site.register(Produto)
--- marsalis/servico/models.py
+++ marsalis/servico/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Usuario(models.Model):
+    identificacao = models.CharField(max_length=15,unique=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.identificacao
+
+
+class Produto(models.Model):
+	descricao = models.CharField(max_length=200,unique=True)
+
+	def __str__(self):
+		return self.descricao
+
+	class Meta:
+		ordering = ['descricao']
